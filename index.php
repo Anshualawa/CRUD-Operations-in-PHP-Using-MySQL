e36cf2ed3539258386564d273e55865aec6c7194
AnshuAlawa <alawapappu@gmail.com>
2023-06-06 15:15:40 +0530

    change file extension html to php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>PHP</title>
 
 </head>
 
